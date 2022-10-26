fdaace5951ac5fd022fc16148b1c4903852fb7d6
Gwardii <62251613+Gwardii@users.noreply.github.com>
2022-10-26 16:29:22 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -27,7 +27,7 @@ class Wektor2D{
             return os;
         }
         void setX(double x){this->x = x;}
-        void setY(double x){this->y = y;}
+        void setY(double y){this->y = y;}
         double getX(){return x;}
         double getY(){return y;}
 };
