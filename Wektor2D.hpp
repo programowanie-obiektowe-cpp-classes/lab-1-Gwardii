49905ce1978fa99958774f6be0ddd62674d7d6f0
Gwardii <62251613+Gwardii@users.noreply.github.com>
2022-10-26 16:11:10 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,20 +1,8 @@
-class Informer{
-    public:
-        Informer(){
-            std::cout << "Wywołano konstruktor" << std::endl;
-        }
-        ~Informer(){
-            std::cout << "Wywołano destruktor" << std::endl;
-        }
-};
-
 class Wektor2D{
     double x;
     double y;
-    Informer informer;
 
     public:
-        static int num_wek;
         Wektor2D(){
             x = 0;
             y = 0;
@@ -22,11 +10,9 @@ class Wektor2D{
         Wektor2D(double x, double y){
             this->x = x;
             this->y = y;
-            std::cout << ++num_wek << std::endl;
         }
         Wektor2D(const Wektor2D& source) : Wektor2D{source.x, source.y}{}
         ~Wektor2D(){
-            this->print();
         }
         double norm(){
             return std::sqrt(x*x + y*y);
@@ -44,10 +30,7 @@ class Wektor2D{
         void setY(double x){this->y = y;}
         double getX(){return x;}
         double getY(){return y;}
-        int getNumWek(){return num_wek;}
 };
-
-int Wektor2D::num_wek = 0;
 
 Wektor2D operator+(Wektor2D w1, Wektor2D w2){
     Wektor2D w{w1.getX() + w2.getX(), w1.getY()+w2.getY()};
